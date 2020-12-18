4793eec79a486d7d0c00fb8710eabfd5c601b4e8
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-18 12:59:44 -0800

    Fix references to lwhen and lunless to their new names

--- src/Control/MonadLeft.purs
+++ src/Control/MonadLeft.purs
@@ -1,6 +1,6 @@
 module Control.MonadLeft where
 
-import Control.ApplicativeLeft (class ApplicativeLeft, lwhen, lunless)
+import Control.ApplicativeLeft (class ApplicativeLeft, whenLeft, unlessLeft)
 import Control.BindLeft (class BindLeft, lbind)
 import Data.Unit (Unit)
 
@@ -9,9 +9,9 @@ class (ApplicativeLeft m, BindLeft m) <= MonadLeft m
 -- | Perform a monadic action when a condition is true, where the conditional
 -- | value is also in a monadic context.
 whenMLeft :: forall m c. MonadLeft m => m Boolean c -> m Unit c -> m Unit c
-whenMLeft mb m = lbind mb \b -> lwhen b m
+whenMLeft mb m = lbind mb \b -> whenLeft b m
 
 -- | Perform a monadic action unless a condition is true, where the conditional
 -- | value is also in a monadic context.
 unlessMLeft :: forall m c. MonadLeft m => m Boolean c -> m Unit c -> m Unit c
-unlessMLeft mb m = lbind mb \b -> lunless b m
+unlessMLeft mb m = lbind mb \b -> unlessLeft b m
