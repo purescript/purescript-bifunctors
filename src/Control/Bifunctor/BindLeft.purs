5efcadf2fc3e97aec7979c36b21ea48dfac5899d
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-08-23 18:06:41 -0700

    Add kind sig to BindLeft

--- src/Control/Bifunctor/BindLeft.purs
+++ src/Control/Bifunctor/BindLeft.purs
@@ -1,20 +1,21 @@
 module Control.Bifunctor.BindLeft where
 
-import Data.Unit (Unit)
+import Control.ApplicativeLeft (class ApplicativeLeft)
 import Control.Category (identity)
+import Data.Unit (Unit)
 
 -- | Same as `Discard` but works on the left parameter of the bifunctor
 -- | rather than the right parameter, which is the default for `Discard`.
 class DiscardLeft a where
-  ldiscard :: forall k (m :: Type -> k -> Type) b r. BindLeft m => m a r -> (a -> m b r) -> m b r
+  ldiscard :: forall m b r. BindLeft m => m a r -> (a -> m b r) -> m b r
 
 instance discardLeftUnit :: DiscardLeft Unit where
   ldiscard = lbind
 
 -- | Same as `Bind` but works on the left parameter of the bifunctor
 -- | rather than the right parameter, which is the default for `Bind`.
-class BindLeft :: forall k. (Type -> k -> Type) -> Constraint
-class BindLeft m where
+class BindLeft :: (Type -> Type -> Type) -> Constraint
+class ApplicativeLeft m <= BindLeft m where
   lbind :: forall a b r. m a r -> (a -> m b r) -> m b r
 
 joinLeft :: forall m a r. BindLeft m => m (m a r) r -> m a r
